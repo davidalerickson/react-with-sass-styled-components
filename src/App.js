264e1cea95c414b6b98995c9fb28ab7e513c8d5a
David Erickson <daviderickson@davids-mbp.lan>
2020-07-05 07:52:41 -0400

    Reformated App.js to be class based. Added sass dir and base, abstract dirs indide. Added corresponding scss files within

--- src/App.js
+++ src/App.js
@@ -2,28 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="turnBlue">
-          This should have a background
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p className="turnBlue">
+            This should have a red background which turns blue when hovered over. Which means that SASS is working 
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
