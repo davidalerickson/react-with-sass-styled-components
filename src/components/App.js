5ff18e552b69c4b0bea73599061723428bc4eb6b
David Erickson <daviderickson@Davids-MacBook-Pro.local>
2020-08-26 15:46:04 -0400

    Cleaned out Repo for a bare bones styled-components Ap

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import Container from './layout/Container';
-import Header from './layout/Header';
-import Content from './layout/Content';
-import Sidebar from './layout/Sidebar';
-import MainPanel from './layout/MainPanel';
-import Gallery from './Gallery';
-import Overview from './layout/Overview';
-import Detail from './layout/Detail';
+
 import { ThemeProvider } from 'styled-components';
 
 class App extends React.Component {
@@ -32,15 +26,7 @@ class App extends React.Component {
           marginTB="8rem"
           maxWidth="120rem"
         >
-                <Header />
-                <Content>
-                  <Sidebar />
-                  <MainPanel>
-                    <Gallery />
-                    <Overview />
-                    <Detail />
-                  </MainPanel>
-                </Content>
+
         </Container>
       </ThemeProvider>
         </>
